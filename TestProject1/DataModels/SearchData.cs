b467d8377e0b426dd444986064e6f1bff61f5b9c
gorbachev <qa.dmitriygorbachev@gmail.com>
2025-08-04 11:52:24 +0700

    commit

---
+++ TestProject1/DataModels/SearchData.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.DevTools.V136.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.DataModels
+{
+    /// <summary>
+    /// Модель данных для работы со страницей контекстного поиска
+    /// </summary>
+    public class SearchData
+    {
+        /// <summary>
+        /// Текст для поиска
+        /// </summary>
+        public string TextForSearching { get; set; }
+        /// <summary>
+        /// Название первой отображаемой публикации
+        /// </summary>
+        public string ExpectedFirstPublicationHeader { get; set; }
+        /// <summary>
+        /// Категория поиска
+        /// </summary>
+        public string ActiveSearchCategory { get; set; }
+    }
+}
